9de4cbad8228ab26beef9053c3275baededa5098
Jyothsna <jyothsnans@gmail.com>
2016-10-21 15:13:09 +0530

    Recreating the classic arcade game Frogger
    Using the visual assts and game engine provided,entities such as the
    player,enemy,etc are added to recreate the arcade game frogger.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+Instructions:
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+To run the game:
+Please open the file index.html in your browser window.
+
+To Play:
+
+1. Aim of the player is to reach the water without colliding with enemies.
+2. Player can move Up,Down,Left and Right using the respective keys(UP,DOWN,LEFT and RIGHT arrow keys)
+3. Player loses when he collides with an enemy bug.
+4. Player wins if he reaches the water without colliding with the enemies.
+
--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
+// Hello.
+//
+// This is JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+//
+// To start, simply enter some JavaScript anywhere on this page. Your
+// report will appear on the right side.
+//
+// Additionally, you can toggle specific options in the Configure
+// menu.
 // Enemies our player must avoid
-var Enemy = function() {
+
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    //Enemies' random speed -- used Math.random as suggested my forum mentor on discussion forum 
+    this.speed = Math.floor((Math.random() * 250) + 100);
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,8 +29,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    this.x += this.speed * dt;
 
+    //If enemy goes out of canvas,reset position of enemy
+    if (this.x > 505) {
+        this.x = 0;
+    }
+};
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -24,14 +44,59 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+//
 
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-cat-girl.png';
+
+};
+//This is to reset the player's position 
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+//This checks for collisions and resets the player position- Collision check function was based on several inputs given by forum mentors and co-students on the 
+//diccussion forum.
+Player.prototype.update = function(dt) {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (this.x < allEnemies[i].x + 50 && this.x + 50 > allEnemies[i].x && this.y < allEnemies[i].y + 37 && this.y + 37 > allEnemies[i].y) {
+            this.reset();
+        }
+    }
+
+    if (this.y > 400) {
+        this.reset();
+    }
+};
+
+//This enables the player to move left,right,up and down
+Player.prototype.handleInput = function(pressedKey) {
+    if (pressedKey === "up" && this.y > 0) {
+        this.y = this.y - 100;
+    } else if (pressedKey === "down" && this.y < 400) {
+        this.y = this.y + 100;
+    } else if (pressedKey === "left" && this.x > 0) {
+        this.x = this.x - 100;
+    } else if (pressedKey === "right" && this.x < 400) {
+        this.x = this.x + 100;
+    }
+    //If player reaches water, reset game and update score
+    if (this.y < 25) {
+        this.reset();
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
-
+var allEnemies = [new Enemy(400, 300), new Enemy(200, 100), new Enemy(100, 200)];
+var player = new Player(400, 400);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
